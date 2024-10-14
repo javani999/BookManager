9fca9a7af7bdbf7f03e58d32ca1af8e131e23efd
javani999 <129900089+javani999@users.noreply.github.com>
2024-10-13 19:26:54 -0500

    finish

--- JDS.BookManager/JDS.BookManager.cpp
+++ JDS.BookManager/JDS.BookManager.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
@@ -18,12 +19,56 @@ struct Book
 
 int main()
 {   
-    Book books[4];
+    Book books[5];
     books[0] = { "The Hobbit", "John RR Tolkien", 1937, 304};
 	books[1] = { "Keeper of the Lost Cities", "Shannon Messenger", 2012, };
     books[2] = { "", "Erin Hunter", 2003, 315 };
     books[3] = { "Eragon", "Christopher Paolini",  503 };
     books[4] = { "Hunter x Hunter", "Yoshihiro Togashi", 1998, 390};
 
+
+	for (int i = 0; i < 5; i++)
+    {
+        cout << "Title: " << books[i].title << "\n";
+		cout << "Author: " << books[i].author << "\n";
+		cout << "Year: " << books[i].year << "\n";
+		cout << "Pages: " << books[i].pages << "\n";
+        cout << "\n";
+    }
+    int index = 0;
+    cout << "Which book info would you like to see? (0-4): "; 
+    cin >> index;
+    if (index >= 0 && index < 5)
+    {
+        cout << "Enter new title: ";
+        cin >> books[index].title;
+        cout << "Enter new author: ";
+        cin >> books[index].author;
+        cout << "Enter new year: ";
+        cin >> books[index].year;
+        cout << "enter new page #: ";
+        cin >> books[index].pages;
+        
+	}
+	else
+	{
+		cout << "Invalid index, that book is not of this world";
+	}
+	cout << "would you like to save changes? (y/n): ";
+    char save;
+	cin >> save;
+    if (save == 'y')
+    {
+		cout << "Title: " << books[index].title << "\n" << "Author: " << books[index].author << "\n" << "Year: " << books[index].year << "\n" << "Pages: " << books[index].pages;
+	}
+	else
+	{
+		cout << "Changes not saved";
+	}
+    
+	
+	
+
+    (void)_getch();
 }
 
